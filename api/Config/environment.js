528201720b6531d4dd2af57aeac9e33f26c58f04
Peter C. Romano <pete@vitruvian.tech>
2017-11-11 14:58:01 -0500

    Move Config api into one fle

--- api/Config/environment.js
+++
@@ -1,6 +0,0 @@
-import async from '@vitruvian-tech/app-studio-core/helpers/Promise';
-import config from '../../../../../config';
-
-export default async((req, params, resolve, reject) => {
-  resolve(config.environment);
-});
--- api/Config/index.js
+++ api/Config/index.js
@@ -1,4 +1,24 @@
-export environment from './environment';
-export layout from './layout';
-export components from './components';
-export api from './api';
+import async from '@vitruvian-tech/app-studio-core/helpers/Promise';
+import config from '../../../../../config';
+
+export const environment = async((req, params, resolve, reject) => {
+    resolve(config.environment);
+});
+
+export const layout = async((req, params, resolve, reject) => {
+    resolve(config.layout);
+});
+
+export const components = async((req, params, resolve, reject) => {
+    resolve(config.settings[req.query.bundle].components || {});
+});
+
+export const api = async((req, params, resolve, reject) => {
+    if (req.headers.host.split(':')[0].toLowerCase() === 'localhost') {
+        resolve(config.settings[req.query.bundle].api || {});
+    } else {
+        const error = new Error('Unauthorized.');
+        error.status = 403;
+        reject(error);
+    }
+});
