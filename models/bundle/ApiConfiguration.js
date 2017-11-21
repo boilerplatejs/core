4ff6089cacfc4f6ef0221ae6b7d3ba4f757e992f
Peter C. Romano <pete@vitruvian.tech>
2017-11-21 13:45:09 -0500

    Update model boilerplate

--- models/bundle/ApiConfiguration.js
+++ models/bundle/ApiConfiguration.js
@@ -1,13 +1,23 @@
-import {parse, define, Model} from '@vitruvian-tech/app-studio-core/lib/Sequelize';
+import {parse, define, Model, DataTypes} from '@vitruvian-tech/app-studio-core/lib/Sequelize';
 @define(parse(__filename))
 
 export default class extends Model {
-    // Example:
-    //
-    // id = {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     primaryKey: true,
-    //     autoIncrement: true
-    // };
+
+    // Attributes:
+
+    /**
+     * Example:
+     *
+     * `id = { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true };`
+     * `name = DataTypes.STRING;`
+     */
+
+    // Descriptors:
+
+    /**
+     * Example:
+     *
+     * `static tableName = parse(__filename); // Use filename as the literal table name`
+     */
+
 };
