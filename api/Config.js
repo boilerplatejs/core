be8134e1266a0b273370219de346f74052f3b371
Peter C. Romano <pete@vitruvian.tech>
2017-11-30 14:45:26 -0500

    Load layout config from database

--- api/Config.js
+++ api/Config.js
@@ -1,19 +1,67 @@
 import async from '@machete-platform/core-bundle/lib/Promise';
 import {getModels} from '@machete-platform/core-bundle/lib/Sequelize';
+import _ from 'lodash';
 import config from '../../../../config';
 
+const PAGE_JSON_KEYS = ['headers', 'sections', 'meta', 'links', 'scripts'];
+const LAYOUT_JSON_KEYS = ['headers', 'sections', 'options'];
+
+const getValues = records => records.map(record => record.dataValues);
+
+const parse = (object, keys) => {
+    keys.forEach(key => {
+        if(object[key] === null) {
+            delete object[key];
+        } else {
+            try {
+                object[key] = JSON.parse(object[key]);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+
+    return object;
+};
+
+const parsePage = page => parse(page, PAGE_JSON_KEYS);
+
+const parseLayout = layout => parse(layout, LAYOUT_JSON_KEYS);
+
+const getLayoutConfig = async () => {
+    const {Layout, MetaTag, Link, Script, Page} = getModels();
+
+    const { app, theme, title, page, headers, sections, options } = await Layout.findAll({ limit: 1, order: [['id', 'DESC']] })
+        .then(getValues)
+        .then(records => records[0]);
+
+    const meta = await MetaTag.findAll()
+        .then(getValues)
+        .then(metatags => metatags.map(({ key, value, content }) => ({ [key]: value, content })))
+        .then(metatags => metatags.map(metatag => metatag.content ? metatag : _.unset(metatag, 'content') && metatag));
+
+    const link = await Link.findAll()
+        .then(getValues)
+        .then(links => links.map(({ rel, href, sizes }) => ({ rel, href, sizes })))
+        .then(links => links.map(link => link.sizes ? link : _.unset(link, 'sizes') && link));
+
+    const script = await Script.findAll()
+        .then(getValues)
+        .then(scripts => scripts.map(({ type, external, content }) => ({ type, [external ? 'src' : 'innerHTML']: content })));
+
+    const pages = await Page.findAll({ order: [['route', 'DESC']] })
+        .then(getValues)
+        .then(pages => pages.map(parsePage))
+        .then(pages => _.keyBy(pages, 'route'))
+        .then(pages => Object.assign(pages, { '$': parseLayout({ title, page, meta, link, script, headers, sections, options }) }));
+
+    return { app, theme, title, pages };
+};
+
+const cache = {};
+
 export const layout = async(async (req, params, resolve, reject) => {
-    const { app, pages, theme } = config;
-
-    if (__DEVELOPMENT__) {
-        console.log('Layout', await getModels().Layout.findAll({ limit: 1, order: [['id', 'DESC']] }).then(records => records[0].dataValues));
-        console.log('MetaTags', await getModels().MetaTag.findAll().then(records => records.map(record => record.dataValues)));
-        console.log('Links', await getModels().Link.findAll().then(records => records.map(record => record.dataValues)));
-        console.log('Scripts', await getModels().Script.findAll().then(records => records.map(record => record.dataValues)));
-        console.log('Pages', await getModels().Page.findAll({ order: [['route', 'DESC']] }).then(records => records.map(record => record.dataValues)));
-    }
-
-    resolve({ app, pages, theme });
+    resolve((cache.layout = cache.layout || await getLayoutConfig()));
 });
 
 export const components = async((req, params, resolve, reject) => {
