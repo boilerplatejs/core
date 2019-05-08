0636de976a9283b4b9b5cc16388c867f04d757f7
Peter C. Romano <pete@vitruvian.tech>
2019-05-08 15:29:31 -0400

    Refactor session service

--- services/Session.js
+++ services/Session.js
@@ -1,18 +1,5 @@
-import async from '@machete-platform/core-bundle/lib/Promise';
+export const load = async req => req.session.user || null;
 
-export const load = async((req, params, resolve) => {
-    resolve(req.session.user || null);
-});
+export const login = async req => (req.session.user = req.body);
 
-export const login = async((req, params, resolve) => {
-    const user = {
-        name: req.body.name
-    };
-    req.session.user = user;
-    resolve(user);
-});
-
-export const logout = async((req, params, resolve) => {
-    req.session = null;
-    resolve(null);
-});
+export const logout = async req => (req.session = null);
