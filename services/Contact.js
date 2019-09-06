14a7350aa4c970f16cd768b2a520ce37a134630c
Peter C. Romano <pete@vitruvian.tech>
2019-09-06 13:47:58 -0400

    Update Contact.js

--- services/Contact.js
+++ services/Contact.js
@@ -2,13 +2,13 @@ import async from '@boilerplatejs/core/lib/Promise';
 import {Contact} from '@boilerplatejs/core/models/default';
 
 export const create = async(async (req, params, resolve, reject) => {
-    const {email, newsletter, quote} = req.body;
+    const {email, newsletter, quote, lead} = req.body;
     const contact = await Contact.findOne({ where: { email } });
 
     req.body.newsletter = req.body.newsletter || false;
 
     try {
-        if (contact && !quote && contact.newsletter && newsletter) {
+        if (contact && !(lead || quote) && contact.newsletter && newsletter) {
             reject(Object.assign(new Error(`The email address "${contact.email}" has already been registered.`), { status: 400 }));
         } else if (contact) {
             req.body.newsletter = contact.newsletter || req.body.newsletter;
